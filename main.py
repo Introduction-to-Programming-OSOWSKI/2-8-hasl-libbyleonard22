83cb637a0ffd0e19b6d3b0a90b3522a2b0be5c20
libbyleonard22 <815989@mystma.org>
2021-10-05 14:57:19 -0500

    2

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #WRITE YOUR CODE IN THIS FILE
 def hasL(w):
-
-    while x < len(w):
-        print(w[c])
+    w == w.lower()
+    for i in range(0, len(w)):
+        print (w[i])
+        
+        return "true"
+    else:
+        return "false"
 
 print(hasL("toronto"))
